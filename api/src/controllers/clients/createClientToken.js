bdfc1a78e9139ae47f5152ae1ebd5d5d0ca0cf8b
Gabriel Villarroel <78510297+RockiJunior@users.noreply.github.com>
2022-02-07 20:23:35 -0300

    some files deleted

--- api/src/controllers/clients/createClientToken.js
+++ api/src/controllers/clients/createClientToken.js
@@ -1,22 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 const createTokenClient = (req, res) => {
-	const user = {
-		nombre: 'Gabriel Vilarroel',
-		clave: '987654321asd',
-		direccion: 'Tucuman 1562',
-		codPostal: '5900',
-		telefono: '3472552354',
-		email: 'gabrielvillarroel_11@hotmail.com',
-		cuit: '20371274198',
-		dni: '37127419',
-		saldo: '450000',
-		nroIngresosBrutos: '123',
-		CtaCte: true,
-		listaDePrecios: '123',
-		permisos: 'admin',
-		condIvaId: 2
-	};
+	const user = req.body;
+
 	jwt.sign({ user }, 'secretkey', (err, token) => {
 		res.json({
 			token
--- api/src/routes/clients/clients.js
+++ api/src/routes/clients/clients.js
@@ -7,7 +7,6 @@ const getClientByName = require('../../controllers/clients/getClientByName.js');
 const createClient = require('../../controllers/clients/createClient.js');
 const modifyClientById = require('../../controllers/clients/modifyClientById.js');
 const modifyClientByName = require('../../controllers/clients/modifyClientByName.js');
-const createClientsToken = require('../../controllers/clients/createClientsToken.js');
 const createClientToken = require('../../controllers/clients/createClientToken.js');
 const verifyClientToken = require('../../controllers/clients/verifyClientToken.js');
 const tokenAuth = require('../../middlewars/tokenAuth.js');
@@ -17,10 +16,7 @@ router.get('/:id', getClientById);
 router.get('/:nombre', getClientByName);
 router.post('/create', createClient);
 router.put('/updateClient/:id', modifyClientById);
-
 router.put('/updateClientByName/:nombre', modifyClientByName);
-
-router.post('/createClientsToken', createClientsToken);
 
 router.post('/createClientToken', createClientToken); //singUp
 router.post('/verifyClientToken', tokenAuth,  verifyClientToken); //logIn
