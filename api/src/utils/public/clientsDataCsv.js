7b59e0a38fdec4e4639fbe8f956197a83e3dda1e
Gabriel Villarroel <78510297+RockiJunior@users.noreply.github.com>
2022-03-17 17:50:16 -0300

    csv library data experiments fails, skipping..

--- api/src/utils/public/clientsDataCsv.js
+++ api/src/utils/public/clientsDataCsv.js
@@ -1,11 +1,31 @@
+// node-csv -----------------------------------------------
 // const fs = require('fs');
-var csv = require('node-csv').createParser();
-
-csv.mapFile('./clientes.csv', (err, data) => {
-    for(let item of data) {
-        
-    }
-});
-
-
+// var csv = require('node-csv').createParser();
+//  csv.mapFile('./productos.csv', (err, data) => {
+//     console.log(data);
+// });
 // fs.createReadStream(__dirname+'/clientes.csv').pipe(csv);
+
+// csv-parser ---------------------------------------------
+
+// const csv = require('csv-parser');
+// const fs = require('fs');
+// fs
+// 	.createReadStream('clientes.csv')
+// 	.pipe(csv())
+// 	.on('data', (row) => {
+// 		console.log(row);
+// 	})
+// 	.on('end', () => {
+// 		console.log('CSV file successfully processed');
+// 	});
+
+// fast-csv ---------------------------------------------
+// const fastcsv = require('fast-csv');
+
+// const fs = require('fs');
+// const ws = fs.createWriteStream("clientes.csv");
+
+// fastcsv
+//   .write(data, { headers: true })
+//   .pipe(ws);
